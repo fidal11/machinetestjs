d5f2a3c9301463deaf723ef6aaf6368f5f59346c
fidal11 <msfidal2001@gmail.com>
2023-05-03 14:26:51 +0530

    first commit

---
+++ electricitybill_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ElectricitybillAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'electricitybill_app'
---
+++ electricitybill_app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+def kseb(request):
+    return render(request,'electricitybill_app/kseb.html')
+ 
